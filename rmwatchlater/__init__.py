f5eb610c9b2312c0aa9d9e11fab4b85346a53a14
tct123 <42028373+tct123@users.noreply.github.com>
2024-12-15 19:22:13 +0100

    .

--- rmwatchlater/__init__.py
+++ rmwatchlater/__init__.py
@@ -7,12 +7,22 @@ from mylocale.TR import tr
 def main():
     url = "https://www.youtube.com/playlist?list=WL"
     trfile = "rmwatcher/localisation.csv"
-    promt = input("Are you logged into your Google Account? ([{yes}]es/[{no}o])".format(yes="y",no="n"))
+    promt = input(
+        "Are you logged into your Google Account? ([{yes}]es/[{no}]o)".format(
+            yes="y", no="n"
+        )
+    )
     webbrowser.open(url)
     try:
-        if promt == tr(csv_file=trfile,target_key="YES"):
+        if promt == tr(csv_file=trfile, target_key="YES"):
             while True:
-                gui.click()
+                gui.click("rmwatchlater/imgs/menue.png")
+        else:
+            print("Error")
 
     except:
         pass
+
+
+if __name__ == "__main__":
+    main()
